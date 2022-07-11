35b2d4d39946ce8e1e4420359f5f11c32dad5896
DESKTOP-4NV9MP5\vantan <sskryo0607@gmail.com>
2022-07-11 20:20:48 +0900

    ひとまず役判定機管理装置を作る

--- Assets/Scripts/PlayerData.cs
+++ Assets/Scripts/PlayerData.cs
@@ -6,8 +6,47 @@ public class PlayerData : MonoBehaviour
 {
     public Biome _biome;
 
-    public void StandUp(Biome b)
+    List<DamyRole> _list = new List<DamyRole>();
+
+    public void StandUp(int b)
     {
-        _biome = b;
+        if(b >= 0 && b <= 3)
+        {
+            _biome = (Biome)b;
+        }
+        else
+        {
+            Debug.LogError("プレイヤーのIDが想定外のナンバーになっている");
+        }
+    }
+}
+
+public class DamyRole
+{
+    Card?[] menbers;
+    int point;
+
+    int checkCount;
+
+    public void Check(Card c)
+    {
+        for(var i = 0; i < menbers.Length; i++)
+        {
+            switch(c.Type)
+            {
+                case CardType.Plant:
+                    if(i == 0 && menbers[i] == null)
+                    {
+                        menbers[i] = c;
+                        checkCount++;
+                    }
+                    break;
+                        
+            }
+        }
+        if (checkCount == menbers.Length)
+        {
+            //pointをプレイヤーの得点に
+        }
     }
 }
