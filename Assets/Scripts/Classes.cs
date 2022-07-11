792a7416c78e1c8a4972b075279d9fb9dcbe9ca0
SR21C <rysawad@gmail.com>
2022-07-11 18:30:47 +0900

    [Update] CardData

--- Assets/Scripts/Classes.cs
+++ Assets/Scripts/Classes.cs
@@ -1,13 +1,39 @@
 public struct Card
 {
-	public Suit Suit;
-	public int Number;
+	public Biorm Suit;
+	public int Number; 
+	public CardType CardType;
 
-	public Card(Suit suit, int number)
+	public Card(Biorm suit, int number)
     {
 		Suit = suit;
 		Number = number;
-    }
+		CardType = CardType.Plant;
+		if(Number > 4 && Number <= 7)
+        {
+			CardType = CardType.Prey;
+		}
+		else if(Number > 7 && Number <= 9)
+        {
+			CardType = CardType.Predator;
+        }
+		else if(Number == 10)
+        {
+			CardType = CardType.PredatorTheTop;
+        }
+		else if(Number == 11)
+        {
+			CardType = CardType.Disaster;
+        }
+		else if(Number == 12)
+        {
+			CardType = CardType.Ark;
+        }
+		else if(Number == 14)
+        {
+
+        }
+	}
 
     public override string ToString()
     {
@@ -15,13 +41,31 @@ public struct Card
     }
 }
 
-public enum Suit
+public enum Biorm
 {
-	Clover,
-	Diamond,
-	Heart,
-	Spade,
+	Savannah,
+	Snowfield,
+	Forest,
+	Ocean
 }
+public enum CardType
+{
+	/// <summary> 植物 </summary>
+	Plant,
+	/// <summary> 被食者 </summary>
+	Prey, 
+	/// <summary> 捕食者 </summary>
+	Predator, 
+	/// <summary> 頂点捕食者 </summary>
+	PredatorTheTop, 
+	/// <summary> 災害 </summary>
+	Disaster, 
+	/// <summary> 方舟 </summary>
+	Ark,
+	/// <summary> ジョーカー summary>
+    Joker
+}
+
 
 /// <summary>
 /// イベント ID 1 とか 2 は PunTurnManager が使っているのでよける
